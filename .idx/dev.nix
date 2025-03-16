09ee7da7046786e33f417c1c5be5272e1c57bad6
nugrahasatria747 <nugrahasatria747@gmail.com>
2025-03-16 18:29:01 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,22 +1,26 @@
-{ pkgs }: {
+{pkgs}: {
   channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
     pkgs.pnpm
   ];
-  idx.extensions = [];
+  idx.extensions = [
+    
+  ];
   idx.previews = {
     previews = {
       web = {
         command = [
-          "pnpm"
-          "install"
-          # "next"
-          # "dev"
-          # "--port"
-          # "$PORT"
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
         ];
-        # manager = "web";
+        manager = "web";
       };
     };
   };
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -5,6 +5,7 @@ import { Revenue } from '@/app/lib/definitions';
 
 // This component is representational only.
 // For data visualization UI, check out:
+
 // https://www.tremor.so/
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
